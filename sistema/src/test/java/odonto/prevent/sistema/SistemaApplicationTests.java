0093bb671f5ab52969283bc9884ba1e7cd156a28
Guilherme Cardoso <65917790+gdcodedev@users.noreply.github.com>
2024-07-05 21:59:21 -0300

    Starting

---
+++ sistema/src/test/java/odonto/prevent/sistema/SistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package odonto.prevent.sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
